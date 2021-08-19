c5ef2540ed702324a80f196dbb0a5a21e17ed200
lacvoss <lacvoss@gmail.com>
2021-08-19 23:55:10 +0000

    adicionando alterações

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def depois_resquest(exc):
+    g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')
+
 @app.route('/hello')
 def pagina_inicial():
     return "Hello World"
